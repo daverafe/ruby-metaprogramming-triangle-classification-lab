1e147353083b4b9b8b393db16ca1fd554e925dde
David Rafe <david.rafe@gmail.com>
2020-12-08 17:12:30 -0500

    commit tree 8ca6fd78636e48a10f59586ca767cd0f2377c36d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3 
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+  def kind
+    # if self.length_1 == self.length_2 && self.length_2 == self.length_3
+    #   :equilateral
+    # elsif self.length_1 == self.length_2 || self.length_1 == self.length_3 || self.length_2 == self.length_3
+    #   :isosceles
+    # else
+    #   :scalene  
+    # end
+    if self.length_1 + self.length_2 <= self.length_3
+      begin
+        raise TriangleError
+      end
+    elsif self.length_1 + self.length_3 <= self.length_2
+      begin
+        raise TriangleError
+      end
+    elsif self.length_2 + self.length_3 <= self.length_1
+      begin
+        raise TriangleError
+      end
+    elsif self.length_1 == self.length_2 && self.length_2 == self.length_3
+        :equilateral
+    elsif self.length_1 == self.length_2 || self.length_1 == self.length_3 || self.length_2 == self.length_3
+        :isosceles
+    else
+      :scalene 
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
